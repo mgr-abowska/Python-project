501a6dcef27732cd417e137ec686a4c87a8f9d96
Maja Grabowska <m.grabowsk4@gmail.com>
2020-06-01 20:09:37 +0200

    ok

--- app.py
+++ app.py
@@ -2,8 +2,7 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import plotly.graph_objs as go
-import pandas as pd
+
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
@@ -12,7 +11,6 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 app.layout = html.Div(children=[
     html.H1(children='Analiza danych z Instagrama', style={
         'textAlign': 'center'}),
-
 
     dcc.Graph(
         id='like-komments-graph',
